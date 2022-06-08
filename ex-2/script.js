10a365bc8c7ab7cd3a947336ec9ae294f3572fc6
larsz13 <lars.ziegler@zhdk.ch>
2022-06-08 16:38:22 +0200

    fixed with Jonas, can read out number now

--- ex-2/script.js
+++ ex-2/script.js
@@ -1,16 +1,54 @@
-//const chosePlayer = document.querySelectorAll("[data-js]");
-const field = document.querySelector('[data-js="box"]');
-//console.log(chosePlayer);
-let vals = [
-    [0,0,0]
-    [0,0,0]
-    [0,0,0]
-];
+var field = [];
+var vals = [0,0,0,0,0,0,0,0,0];
+var turn = "orange";
 
-function myFunction(){
-    console.log("clicked");
-    field.classList.toggle("blue");
+
+
+for(var x=0;x<9;x++){
+    field[x] = document.querySelector('[data-js="'+x+'"]');
+    console.log(field[x]);
+    //field[x].addEventListener('click', updateVal);
+    field[x].addEventListener('click', updateVal);
     
 }
 
-field.addEventListener("click", myFunction);
+function updateVal(nr){
+    console.log(nr.target.attributes['data-js']);
+    console.log(nr.target.dataset.js);
+
+    if(turn=="orange" && vals[0]==0){
+        //vals[nr] =  vals[nr] - 1;
+        vals[0] = vals[0]-1;
+        field[0].classList.toggle("orange");
+        turn = "blue";
+    }else if(turn=="blue" && vals[0]==0){
+        //vals[nr] = vals[nr] + 1;
+        vals[0] = vals[0]+1;
+        field[0].classList.toggle("blue");
+        turn = "orange";
+    }
+    winCheck();
+};
+
+
+function winCheck(){
+    if( vals[0]+vals[1]+vals[2]==-3 ||
+        vals[3]+vals[4]+vals[5]==-3 ||
+        vals[6]+vals[7]+vals[8]==-3 ||
+        vals[0]+vals[3]+vals[6]==-3 ||
+        vals[1]+vals[4]+vals[7]==-3 ||
+        vals[2]+vals[5]+vals[8]==-3
+        ){
+        //orangeWin();
+        console.log("ORANGE WIN");
+    }else if(
+        vals[0]+vals[1]+vals[2]==3 ||
+        vals[3]+vals[4]+vals[5]==3 ||
+        vals[6]+vals[7]+vals[8]==3 ||
+        vals[0]+vals[3]+vals[6]==3 ||
+        vals[1]+vals[4]+vals[7]==3 ||
+        vals[2]+vals[5]+vals[8]==3
+    ){
+        //blueWin();
+        console.log("BLUE WIN");
+}}
