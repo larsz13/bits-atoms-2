0d685d3814de9134d54ee9ee8b6643c1b36f887f
larsz13 <lars.ziegler@zhdk.ch>
2022-06-09 15:22:17 +0200

    game works

--- ex-2/script.js
+++ ex-2/script.js
@@ -1,30 +1,48 @@
 var field = [];
+const restart = document.querySelector('[data-js="restart"]');
 var vals = [0,0,0,0,0,0,0,0,0];
 var turn = "orange";
-
+const gameText = document.getElementById("turn")
+var player1 = "sven";
+var player2 = "mike";
 
 
 for(var x=0;x<9;x++){
     field[x] = document.querySelector('[data-js="'+x+'"]');
-    console.log(field[x]);
-    //field[x].addEventListener('click', updateVal);
-    field[x].addEventListener('click', updateVal);
-    
+}
+initialiseGame();
+
+
+function initialiseGame(){
+    for(var x=0;x<9;x++){
+        field[x].addEventListener('click', updateVal);
+        if(vals[x]==-1){
+            field[x].classList.toggle("orange");
+            console.log("new_blue");
+        }else if(vals[x]==1){
+            field[x].classList.toggle("blue");
+            console.log("new_orange");
+        }
+        vals[x] = 0;
+    }
 }
 
+restart.addEventListener('click',initialiseGame);
+
 function updateVal(nr){
-    console.log(nr.target.attributes['data-js']);
     console.log(nr.target.dataset.js);
 
-    if(turn=="orange" && vals[0]==0){
+    if(turn=="orange" && vals[nr.target.dataset.js]==0){
         //vals[nr] =  vals[nr] - 1;
-        vals[0] = vals[0]-1;
-        field[0].classList.toggle("orange");
+        vals[nr.target.dataset.js] = vals[nr.target.dataset.js]-1;
+        field[nr.target.dataset.js].classList.toggle("orange");
+        gameText.innerHTML = player1+"'s turn";
         turn = "blue";
-    }else if(turn=="blue" && vals[0]==0){
+    }else if(turn=="blue" && vals[nr.target.dataset.js]==0){
         //vals[nr] = vals[nr] + 1;
-        vals[0] = vals[0]+1;
-        field[0].classList.toggle("blue");
+        vals[nr.target.dataset.js] = vals[nr.target.dataset.js]+1;
+        field[nr.target.dataset.js].classList.toggle("blue");
+        gameText.innerHTML = player2+"'s turn";
         turn = "orange";
     }
     winCheck();
@@ -37,18 +55,43 @@ function winCheck(){
         vals[6]+vals[7]+vals[8]==-3 ||
         vals[0]+vals[3]+vals[6]==-3 ||
         vals[1]+vals[4]+vals[7]==-3 ||
-        vals[2]+vals[5]+vals[8]==-3
+        vals[2]+vals[5]+vals[8]==-3 ||
+        vals[0]+vals[4]+vals[8]==-3 ||
+        vals[2]+vals[4]+vals[6]==-3
         ){
-        //orangeWin();
-        console.log("ORANGE WIN");
+        for(var x=0;x<9;x++){
+            field[x].removeEventListener('click', updateVal);
+        }
+        gameText.innerHTML = player2+" wins!";
+
     }else if(
         vals[0]+vals[1]+vals[2]==3 ||
         vals[3]+vals[4]+vals[5]==3 ||
         vals[6]+vals[7]+vals[8]==3 ||
         vals[0]+vals[3]+vals[6]==3 ||
         vals[1]+vals[4]+vals[7]==3 ||
-        vals[2]+vals[5]+vals[8]==3
+        vals[2]+vals[5]+vals[8]==3 ||
+        vals[0]+vals[4]+vals[8]==3 ||
+        vals[2]+vals[4]+vals[6]==3
     ){
-        //blueWin();
         console.log("BLUE WIN");
-}}
+        for(var x=0;x<9;x++){
+            field[x].removeEventListener('click', updateVal);
+        }
+        gameText.innerHTML = player1+" wins!";
+    }else if(   vals[0]!=0 &&
+                vals[1]!=0 &&
+                vals[2]!=0 &&
+                vals[3]!=0 &&
+                vals[4]!=0 &&
+                vals[5]!=0 &&
+                vals[6]!=0 &&
+                vals[7]!=0 &&
+                vals[8]!=0
+        ){
+            for(var x=0;x<9;x++){
+                field[x].removeEventListener('click', updateVal);
+            }
+            gameText.innerHTML = "it's a tie!";
+        }
+}
